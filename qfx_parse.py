fc96cee846bd5b82a189190878980a15d06f7e32
Jacob Locke <jacoblocke21@live.com>
2022-05-18 13:38:07 -0700

    Add function for parsing file into list of statements

--- qfx_parse.py
+++ qfx_parse.py
@@ -5,15 +5,42 @@ import re
 import os
 
 
+######################
+### declares
+######################
+pattern_statement = "\<STMTTRN\>.*?\<\/STMTTRN\>"
 
+######################
+### functions
+######################
+def read_all_text(file_name):
+    text = ""
+    with open(file_name, "r") as file:
+        text = file.read()
+    return text
 
+def build_statement_list(file_name, pattern):
+    text = read_all_text(file_name)
+    matches = re.findall(pattern, text)
+    return matches
 
-
-
+######################
+### main
+######################
 parser = argparse.ArgumentParser()
+parser.add_argument("file_name", type=str)
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 
 args = parser.parse_args()
 
 if args.verbose:
     print("verbosity turned on")
+
+
+file_name = os.path.realpath(args.file_name)
+statements = build_statement_list(file_name, pattern_statement)
+i = 0
+for statement in statements:
+    print("[" + str(i) + "] : " + statement)
+    i += 1
+
