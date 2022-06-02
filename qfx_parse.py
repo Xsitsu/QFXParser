d3ca74306ca42cf68e3bf83af3c86d245f8fa304
Jacob Locke <jacoblocke21@live.com>
2022-06-02 14:12:30 -0700

    Add function for parsing into <> tags

--- qfx_parse.py
+++ qfx_parse.py
@@ -8,7 +8,12 @@ import os
 ######################
 ### declares
 ######################
-pattern_statement = "\<STMTTRN\>.*?\<\/STMTTRN\>"
+pattern = {
+    "general": "\<.*?>[^\<]*",
+    "statement": "\<STMTTRN\>.*?\<\/STMTTRN\>"
+}
+
+
 
 ######################
 ### functions
@@ -24,6 +29,20 @@ def build_statement_list(file_name, pattern):
     matches = re.findall(pattern, text)
     return matches
 
+def DBG_print_statement(i, statement):
+	print("[" + str(i) + "] : " + statement)
+
+def DBG_print_statement_list(statement_list):
+    i = 0
+    for statement in statements:
+        DBG_print_statement(i, statement)
+
+def test(statement_text, pattern):
+    matches = re.findall(pattern, statement_text)
+    for mat in matches:
+        print(mat)
+
+
 ######################
 ### main
 ######################
@@ -38,9 +57,7 @@ if args.verbose:
 
 
 file_name = os.path.realpath(args.file_name)
-statements = build_statement_list(file_name, pattern_statement)
-i = 0
-for statement in statements:
-    print("[" + str(i) + "] : " + statement)
-    i += 1
+statements = build_statement_list(file_name, pattern["statement"])
 
+DBG_print_statement(0, statements[0])
+test(statements[0], pattern["general"])
