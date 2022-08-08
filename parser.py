142e8473b264a5e569f27ef76349dbd2dfb3a15c
Jacob Locke <jacoblocke21@live.com>
2022-08-07 19:14:29 -0700

    Fix bug with venmo csv parsing. Also filter out venmo transactions from qfx

--- parser.py
+++ parser.py
@@ -35,24 +35,33 @@ class Parser:
 
     def _dollar_to_float(self, t):
         text = t.replace(" ", "")
+        text = text.replace(",", "")
         if text[0] == "-":
             return -float(text[2:])
         else:
             return float(text[2:])
 
+    def _parse_qfx_statement(self, stat):
+        dct = self._build_qfx_dict(stat)
+        if dct["NAME"].find("VENMO") != -1:
+            return
+
+        trn = transaction.Transaction()
+        self.transactions.append(trn)
+
+        self._try_assign(str, trn, dct, "TRNTYPE", "type")
+        self._try_assign(str, trn, dct, "DTPOSTED", "date_posted")
+        self._try_assign(float, trn, dct, "TRNAMT", "amount")
+        self._try_assign(str, trn, dct, "FITID", "fitid")
+        self._try_assign(str, trn, dct, "NAME", "name")
+        self._try_assign(str, trn, dct, "MEMO", "memo")
+
+
+
     def parse_qfx(self, text_data):
         statements = re.findall(PATTERN_STATEMENT, text_data)
         for stat in statements:
-            trn = transaction.Transaction()
-            self.transactions.append(trn)
-
-            dct = self._build_qfx_dict(stat)
-            self._try_assign(str, trn, dct, "TRNTYPE", "type")
-            self._try_assign(str, trn, dct, "DTPOSTED", "date_posted")
-            self._try_assign(float, trn, dct, "TRNAMT", "amount")
-            self._try_assign(str, trn, dct, "FITID", "fitid")
-            self._try_assign(str, trn, dct, "NAME", "name")
-            self._try_assign(str, trn, dct, "MEMO", "memo")
+            self._parse_qfx_statement(stat)
 
     def parse_venmo(self, file_name):
         with open(file_name, newline='') as file:
